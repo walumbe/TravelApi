b32fa5a6ebe7666f560dde51f1d0b747a33d0bc8
walumbe <nathanwalumbe@gmail.com>
2023-06-29 13:22:06 +0300

    travel Api

---
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TravelResource;
+use Illuminate\Http\Request;
+use App\Models\Travel;
+
+class TravelController extends Controller
+{
+    public function index()
+    {
+        $travels = Travel::where('is_public', true)->paginate();
+
+        return TravelResource::collection($travels);
+    }
+}
